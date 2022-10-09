fd8d52792b9d5bd3bc58ba036767173559ac9134
monasimo <sim.navickaite@gmail.com>
2022-10-09 17:07:06 +0300

    4th task completed,JS and HTML (bootstrap added) updated.

--- Task 4/script.js
+++ Task 4/script.js
@@ -10,3 +10,74 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+let output = document.getElementById("output")
+
+fetch('cars.json')
+    .then(x => x.json())
+    .then((data) => {
+
+        for (i=0; i<data.length; i++) {
+            createCard(data[i])
+        }
+
+        function createCard () {
+
+            let card = document.createElement('div')
+            card.className = 'card container'
+            card.style = 'width:18rem; margin-top:1rem'
+            output.appendChild(card)
+
+            let brand = document.createElement('div')
+            brand.className = 'card-header'
+            brand.style = 'width:18rem'
+            brand.innerText = data[i].brand
+
+            let modelsBox = document.createElement('ul')
+            modelsBox.className = 'list-group list-group-flush'
+            card.appendChild(brand)
+            card.appendChild(modelsBox)
+
+            // console.log(data[0].models)
+            
+            const models = document.createElement('li')
+            models.className = 'list-group-item'
+            models.style = 'width:17rem'
+            models.innerText = data[i].models
+            modelsBox.appendChild(models)
+            
+        }
+    })
+
+   
+
+
+
+
+       
+    //     for (i=0; i<data[i].models.length; i++) {
+
+    //         let models = data[i].models // console.log(data[i].models)
+
+
+    //         // console.log(models)
+
+    //         const categoryItem = document.createElement('li')
+    //     categoryItem.className = 'list-group-item'
+    //     categoryItem.innerText = models
+    //     modelsBox.appendChild(categoryItem)
+
+
+    //     }
+    // }
+
+
+
+// <div class="card" style="width: 18rem;">
+//   <div class="card-header">
+//     Featured
+//   </div>
+//   <ul class="list-group list-group-flush">
+//     <li class="list-group-item">An item</li>
+//   </ul>
+// </div>
