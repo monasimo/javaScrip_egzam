7f47663f9036f331d76403f2bafb935ea360105a
monasimo <sim.navickaite@gmail.com>
2022-10-10 23:42:43 +0300

    9th task, JS updated

--- Task 9/script.js
+++ Task 9/script.js
@@ -6,3 +6,21 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kiru atveju false 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor(title, director, budget) {
+      this.title = title;
+      this.director = director;
+      this.budget = budget;}
+      wasExpensive = function () {
+         return this.budget>100000000 ? true :false
+        } 
+    }
+  
+let MyBudget = new Movie ('Sostu karai', 'Direktorius as', 1000000000)
+
+console.log(MyBudget)
+
+console.log('Biudzeto info: ' + MyBudget.wasExpensive)
+
+ // nelabai gavos :|
