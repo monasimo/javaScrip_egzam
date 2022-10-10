caec3e5547c5362e614112bed3e1d19e67f80d13
monasimo <sim.navickaite@gmail.com>
2022-10-10 16:56:05 +0300

    8th task completed, JS updated

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,24 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator(first, second) {
+    this.firstNumber = first;
+    this.secondNumber = second;
+    this.sum = function () {
+        return this.firstNumber + this.secondNumber
+    };
+    
+    this.subtraction = function () {
+        return this.firstNumber - this.secondNumber
+    };
+
+    this.multiplication = function () {
+        return this.firstNumber * this.secondNumber
+    };
+
+    this.division = function () {
+        return this.firstNumber / this.secondNumber
+    };
+}
+
